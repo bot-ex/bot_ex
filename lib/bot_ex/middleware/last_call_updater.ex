2e06d909b0c6d285e90abb1a4e55113647ecd723
drilla <drilla@mail.ru>
2020-04-14 15:09:56 +0300

    fix

--- lib/bot_ex/middleware/last_call_updater.ex
+++ lib/bot_ex/middleware/last_call_updater.ex
@@ -8,15 +8,14 @@ defmodule BotEx.Middleware.LastCallUpdater do
   alias BotEx.Models.Message
   alias BotEx.Helpers.UserActions
 
+  @doc """
+  Save last user message to ets table
+  """
   @spec transform(Message.t()) :: Message.t()
   def transform(%Message{is_cmd: false} = t_msg) do
     t_msg
   end
   
-  @doc """
-  Save last user message to ets table
-  """
-  @spec transform(Message.t()) :: Message.t()
   def transform(%Message{user_id: user_id} = t_msg) do
     UserActions.update_last_call(user_id, t_msg)
 
